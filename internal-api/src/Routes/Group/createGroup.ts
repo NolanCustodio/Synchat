f84145aa4c3a5563cfca9b3240b3d89e849d11b3
NolanCustodio <nolanvongoop28@gmail.com>
2024-03-18 15:00:23 -0400

    trying to make group, problems with table relations

--- internal-api/src/Routes/Group/createGroup.ts
+++ internal-api/src/Routes/Group/createGroup.ts
@@ -8,5 +8,42 @@ export default async function createGroup(data: any){
     
     console.log(data);
 
+    const datetimeISO = `${data.startDate} ${data.startTime.hour}:${data.startTime.minute}:00.000`;
+
+    try{
+        const newGroup = await prisma.group.create({
+            data:{
+                groupName: data.groupName,
+                events: {
+                    create:[
+                        {
+                            eventName: data.eventName,
+                            startDate: datetimeISO
+                        }
+                    ]
+                }
+            }
+        })
+
+        console.log('new group', newGroup);
+        
+        // const update = await prisma.group.update({
+        //     where: { id: newGroup.id },
+        //     data:{
+        //         users:{
+        //             connect:[
+        //                 {userId: data.userId}
+        //             ]
+        //         }
+        //     }
+        // })
+
+        // console.log('update', update);
+
+    }catch(error){
+
+    }
+
+
     return rtnData;
 }
